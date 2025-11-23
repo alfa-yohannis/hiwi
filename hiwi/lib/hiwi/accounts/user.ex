c84a95bc876d21b22533d988f989a19be944c29e
Raijinnnn <erdine73@gmail.com>
2025-11-24 02:29:50 +0700

    Gabung fitur USR-REG (Nopal) & OWN-CRUD (Erdine), fix dependencies & router

--- hiwi/lib/hiwi/accounts/user.ex
+++ hiwi/lib/hiwi/accounts/user.ex
@@ -1,67 +1,124 @@
 defmodule Hiwi.Accounts.User do
   use Ecto.Schema
   import Ecto.Changeset
-
-  # === INI PERBAIKANNYA (YANG HILANG) ===
-  # Kita butuh `Pbkdf2` (dari library pbkdf2_elixir), BUKAN `Comeonin.Pbkdf2`
-  # Modul ini ada karena Anda sudah install `pbkdf2_elixir` di mix.exs [cite: hiwi/mix.exs]
   alias Pbkdf2
 
   schema "users" do
-    # Field yang dibutuhkan untuk registrasi (sesuai README)
     field :full_name, :string
+    field :email, :string
     field :phone_number, :string
     
-    # Field dari kode Nopal (untuk Pbkdf2)
-    field :email, :string
-    field :hashed_password, :string
     field :password, :string, virtual: true, redact: true
-    field :password_confirmation, :string, virtual: true, redact: true
+    field :hashed_password, :string, redact: true
+    field :current_password, :string, virtual: true, redact: true
     field :confirmed_at, :utc_datetime
 
     timestamps(type: :utc_datetime)
   end
 
   @doc """
-  Changeset untuk registrasi user baru (USR-REG Nopal).
-  INI JUGA PERBAIKANNYA: Menambahkan full_name, phone_number, password_confirmation
+  A user changeset for registration.
   """
-  def registration_changeset(user, attrs) do
+  def registration_changeset(user, attrs, opts \\ []) do
     user
-    |> cast(attrs, [:full_name, :phone_number, :email, :password, :password_confirmation])
-    |> validate_required([:full_name, :phone_number, :email, :password, :password_confirmation])
-    |> validate_format(:email, ~r/@/i)
-    |> validate_length(:password, min: 8, max: 72)
-    |> validate_confirmation(:password)
-    |> unsafe_validate_unique(:email, Hiwi.Repo)
-    |> unique_constraint(:email)
-    |> put_hashed_password() # Memanggil fungsi internal di bawah
+    |> cast(attrs, [:email, :password, :full_name, :phone_number])
+    |> validate_email(opts)
+    |> validate_password(opts)
+    |> validate_required([:full_name, :phone_number]) # Wajib isi nama & HP
   end
 
-  # --- Fungsi Internal untuk Password Hashing (dari kode Nopal) ---
+  defp validate_email(changeset, opts) do
+    changeset
+    |> validate_required([:email])
+    |> validate_format(:email, ~r/^[^\s]+@[^\s]+$/, message: "must have the @ sign and no spaces")
+    |> validate_length(:email, max: 160)
+    |> maybe_validate_unique_email(opts)
+  end
 
-  defp put_hashed_password(changeset) do
-    case changeset do
-      %Ecto.Changeset{valid?: true, changes: %{password: password}} ->
-        # Memanggil alias `Pbkdf2` (yang sudah benar)
-        put_change(changeset, :hashed_password, Pbkdf2.hash_pwd_salt(password))
+  defp validate_password(changeset, opts) do
+    changeset
+    |> validate_required([:password])
+    |> validate_length(:password, min: 6, max: 72)
+    |> maybe_hash_password(opts)
+  end
 
-      _ ->
-        changeset
+  defp maybe_hash_password(changeset, opts) do
+    hash_password? = Keyword.get(opts, :hash_password, true)
+    password = get_change(changeset, :password)
+
+    if hash_password? && password && changeset.valid? do
+      changeset
+      |> put_change(:hashed_password, Pbkdf2.hash_pwd_salt(password))
+      |> delete_change(:password)
+    else
+      changeset
+    end
+  end
+
+  defp maybe_validate_unique_email(changeset, opts) do
+    if Keyword.get(opts, :validate_email, true) do
+      changeset
+      |> unsafe_validate_unique(:email, Hiwi.Repo)
+      |> unique_constraint(:email)
+    else
+      changeset
     end
   end
 
   @doc """
-  Verifikasi password (dari kode Nopal).
+  A user changeset for changing the email.
+  """
+  def email_changeset(user, attrs, opts \\ []) do
+    user
+    |> cast(attrs, [:email])
+    |> validate_email(opts)
+    |> case do
+      %{changes: %{email: _}} = changeset -> changeset
+      %{} = changeset -> add_error(changeset, :email, "did not change")
+    end
+  end
+
+  @doc """
+  A user changeset for changing the password.
+  """
+  def password_changeset(user, attrs, opts \\ []) do
+    user
+    |> cast(attrs, [:password])
+    |> validate_confirmation(:password, message: "does not match password")
+    |> validate_password(opts)
+  end
+
+  @doc """
+  Confirms the account by setting `confirmed_at`.
+  """
+  def confirm_changeset(user) do
+    now = DateTime.utc_now() |> DateTime.truncate(:second)
+    change(user, confirmed_at: now)
+  end
+
+  @doc """
+  Verifies the password.
   """
   def valid_password?(%Hiwi.Accounts.User{hashed_password: hashed_password}, password)
       when is_binary(hashed_password) and byte_size(password) > 0 do
-    # Memanggil alias `Pbkdf2` (yang sudah benar)
     Pbkdf2.verify_pass(password, hashed_password)
   end
 
   def valid_password?(_, _) do
-    Pbkdf2.no_user_verify() # Memanggil alias `Pbkdf2` (yang sudah benar)
+    Pbkdf2.no_user_verify()
     false
+  end
+
+  @doc """
+  Validates the current password otherwise adds an error to the changeset.
+  """
+  def validate_current_password(changeset, password) do
+    changeset = cast(changeset, %{current_password: password}, [:current_password])
+
+    if valid_password?(changeset.data, password) do
+      changeset
+    else
+      add_error(changeset, :current_password, "is not valid")
+    end
   end
 end
